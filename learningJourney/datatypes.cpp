5dd86a0311e12e52d6ae0213df0138b27b16d91d
heyameerhamza <ameerhamza9356@outlook.com>
2026-09-02 22:29:13 -0700

    i added datatypes I learnt today!

---
+++ learningJourney/datatypes.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main() {
+
+    // integers (whole numbers)
+    int age = 20;
+    int year = 2026;
+
+    // floats (decimal numbers)
+    double price = 10.50;
+    double gpa = 3.7;
+
+    // single character
+    char curruncy = '$';
+    char left = '>';
+
+    // boolean
+    bool isTrue  = true;
+    bool varified = true;
+    bool minor = false;
+
+    // strings (sequence of texts)
+
+    std::string name = "Hamza";
+
+
+    // output
+    std::cout << "Hello! I'm "<< name << "\n";
+    std::cout << "My age is "<< age << "\n";
+    std::cout << "I have "<< gpa << " in my class!" << "\n";
+
+    return 0;
+}
