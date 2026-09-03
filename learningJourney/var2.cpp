5312bf05a892b2b5d1786fcc09c0b74501236bc5
heyameerhamza <ameerhamza9356@outlook.com>
2026-09-02 21:43:47 -0700

    added var2.cpp seconf method

---
+++ learningJourney/var2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+    int dif = x - y;
+
+    std::cout << "This is sum of x & y is: " << sum <<"!" << "\n";
+    std::cout << "This is dif of x & y is: " << dif <<"!" << "\n";
+    return 0;
+}
